5241ebb910093b66980a37898555dfe09ebf3491
Harmony Trevena <harmonytrevena@gmail.com>
2020-10-08 15:10:38 -0400

    Completed API and React Exercise

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import RandomUser from "./components/RandomUser.jsx";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    userData: [],
+    tick: 0,
+  };
+
+  loadData = async () => {
+    const response = await fetch("https://randomuser.me/api/?results=10");
+    const data = await response.json();
+    console.log("data is", data);
+    return data;
+  }
+
+  handleClick = async () => {
+    const userData = await this.loadData();
+    this.setState({
+      userData: userData.results,
+    });
+  }
+
+  async componentDidMount() {
+    console.log("The component mounted");
+    const userData = await this.loadData();
+    this.setState({
+      userData: userData.results,
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Random User</h1>
+        </header>
+        <button onClick={this.handleClick}>Load More Users</button>
+          {this.state.userData.length ? (
+          <RandomUser userData={this.state.userData} />
+          ) : (
+          <p>No User Data</p>
+          )}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/RandomUser.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./RandomUser.css";
+
+const RandomUser = (props) => {
+    const { userData } = props;
+    return (
+        <ul>
+            {userData.map((user) => (
+                <li className="user-card">
+                    <img src={user.picture.large} alt={`{user.name.first} {user.name.last}`}/>
+                    <p>
+                        {user.name.first} {user.name.last}
+                    </p>
+                </li>
+            ))}
+        </ul>
+    );
+}
+export default RandomUser;
